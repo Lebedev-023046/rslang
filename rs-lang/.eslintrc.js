bf0d86b76cdb5efb28aedd2fd25aea4b0a164a27
lebedev-023046 <lebedev.023046@gmail.com>
2022-08-19 00:32:29 +0300

    feat: add eslint

---
+++ rs-lang/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
+}
