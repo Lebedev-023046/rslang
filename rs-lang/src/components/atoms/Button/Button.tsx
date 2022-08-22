bd5b94825396dd41c6a8edbe44a2f61d0b6f95b0
Aldar <aldarkonov@mail.ru>
2022-08-23 02:18:17 +0300

    feat: add Button atom component

---
+++ rs-lang/src/components/atoms/Button/Button.tsx
@@ -0,0 +1,68 @@
+import './Button.css'
+import React from 'react'
+
+interface ButtonProps {
+  text: string
+  type: 'primary' | 'secondary' | 'bordered' | 'rounded'
+  small: boolean
+  disabled: boolean
+  // TODO add additional icon
+  iconSrc?: string
+  onClick: () => void
+}
+
+const Button = ({
+  text = 'button',
+  type = 'primary',
+  small = false,
+  disabled = false,
+  iconSrc = '',
+  onClick = () => {}
+}: ButtonProps) => {
+  const sizeClass = small ? 'button_small' : 'button_big'
+
+  switch (type) {
+    case 'primary':
+      return (
+        <button
+          disabled={disabled}
+          className={`button button_primary ${sizeClass}`}
+          onClick={onClick}
+        >
+          {text}
+        </button>
+      )
+    case 'secondary':
+      return (
+        <button
+          disabled={disabled}
+          className={`button button_secondary ${sizeClass}`}
+          onClick={onClick}
+        >
+          {text}
+        </button>
+      )
+    case 'bordered':
+      return (
+        <button
+          disabled={disabled}
+          className={`button button_bordered ${sizeClass}`}
+          onClick={onClick}
+        >
+          {text}
+        </button>
+      )
+    case 'rounded':
+      return (
+        <button
+          disabled={disabled}
+          className={`button button_rounded ${sizeClass}`}
+          onClick={onClick}
+        >
+          {text}
+        </button>
+      )
+  }
+}
+
+export default Button
