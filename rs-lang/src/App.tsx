4cddfe6f3342d5271d4babf2c8b20afa24723fa2
lebedev-023046 <lebedev.023046@gmail.com>
2022-09-05 17:31:15 +0300

    feat: add TextBook link

--- rs-lang/src/App.tsx
+++ rs-lang/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import MainPage from './components/templates/MainPage/MainPage'
 import AudioChallengePage from './components/templates/AudioChallengePage/AudioChallengePage'
 import { Route, Routes } from 'react-router-dom'
+import { TextBook } from './components/templates/TextBook/TextBook'
 
 const App = () => {
   return (
     <Routes>
         <Route path='/' element={ <MainPage /> } />
         <Route path='/AudioChallenge' element={ <AudioChallengePage /> } />
+        <Route path='/TextBook' element={ <TextBook /> } />
     </Routes>
   )
 }
