6a35686442f55de205e7d2844747407534794819
Aldar <aldarkonov@mail.ru>
2022-09-05 21:50:24 +0300

    feat: add adaptivity to WhyUs

--- rs-lang/src/components/organisms/WhyUs/WhyUs.tsx
+++ rs-lang/src/components/organisms/WhyUs/WhyUs.tsx
@@ -30,7 +30,7 @@ const WhyUs: React.FC = () => {
           />
         </div>
         <div className='why-us__content'>
-          <h2>Why choose us?</h2>
+          <h2 className='why-us__heading'>Why choose us?</h2>
           <p className='why-us__text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
           <Button
             type='bordered'
