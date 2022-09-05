05813611b614b7c40bc7f762df79b431f0ec877f
lebedev-023046 <lebedev.023046@gmail.com>
2022-09-05 17:32:16 +0300

    feat: add color classes to wordCards

--- rs-lang/src/components/molecules/WordCard/WordCard.tsx
+++ rs-lang/src/components/molecules/WordCard/WordCard.tsx
@@ -1,14 +1,30 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import './WordCard.css'
 import soundImg from '../../../assets/sound_icon.svg'
 import { IData } from '../../../interfaces/IData'
+import { useContext } from 'react'
+import { authContext } from '../../../context/AuthContext/AuthContext'
+// import Api from '../../../api/Api'
 
 const BASE_URL = 'https://react-rslang-words.herokuapp.com/'
 
 interface ICardProps {
     card: IData
+    active: number
 }
 
+const colors = [
+    'beginner-bg-color',
+    'elementary-bg-color',
+    'intermediate-bg-color',
+    'upper-bg-color',
+    'advanced-bg-color',
+    'proficiency-bg-color',
+    'difficult-bg-color'
+]
+
 export function WordCard (props: ICardProps) {
+    const { isAuth } = useContext(authContext)
     const playWordSounds = () => {
         const audios = [props.card.audio, props.card.audioMeaning, props.card.audioExample].map((path: string) => new Audio(`${BASE_URL}${path}`))
         const playItem = async (audio: HTMLAudioElement) => await new Promise<void>((resolve) => {
@@ -20,12 +36,11 @@ export function WordCard (props: ICardProps) {
                 await playItem(audios[i])
             }
         }
-
         void subsPlay()
     }
 
     return (
-        <div className="word-card-container">
+        <div id={ props.card._id } className={ `word-card-container ${props.active !== 6 ? colors[props.active] : ''}`}>
             <div className="card-img" style={{ backgroundImage: `url(${BASE_URL}${props.card.image})` }}/>
             <div className="card-info">
                 <div className="card-info-block">
@@ -49,10 +64,10 @@ export function WordCard (props: ICardProps) {
                             <p className="sentense-translate2" dangerouslySetInnerHTML={{ __html: props.card.textExampleTranslate }}></p>
                         </div>
                     </div>
-                    <div className="word-buttons">
-                        <button className='btnWordCard'>Mark as learned</button>
+                    { isAuth && <div className="word-buttons">
+                        <button className='btnWordCard'>Mark as learning</button>
                         <button className='btnWordCard'>Add to difficult words</button>
-                    </div>
+                    </div>}
                 </div>
             </div>
         </div>
