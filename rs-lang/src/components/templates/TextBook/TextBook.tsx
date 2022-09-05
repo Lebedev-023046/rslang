22f66a225a39a278ee484bc16ba8d04039c7b5bf
lebedev-023046 <lebedev.023046@gmail.com>
2022-09-05 17:33:51 +0300

    refactor: change getWords to getAggregatedWords

--- rs-lang/src/components/templates/TextBook/TextBook.tsx
+++ rs-lang/src/components/templates/TextBook/TextBook.tsx
@@ -1,31 +1,44 @@
-import { Pagination } from '@mui/material'
 import { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import Api from '../../../api/Api'
+import { authContext } from '../../../context/AuthContext/AuthContext'
 import { signInUpContext } from '../../../context/ModalContext/ModalContext'
 import { IData } from '../../../interfaces/IData'
 import Button from '../../atoms/Button/Button'
-import { ButtonLevelsBlock } from '../../molecules/ButtonLevelsBlock/ButtonLevelsBlock'
-import { CardSkeletonContainer } from '../../molecules/CardSkeleton/CardSkeleton'
+import { NameBlock } from '../../atoms/NameBlock/NameBlock'
 import Nav from '../../molecules/Nav/Nav'
 import Footer from '../../organisms/Footer/Footer'
+import Games from '../../organisms/Games/Games'
 import Header from '../../organisms/Header/Header'
 import { Modal } from '../../organisms/Modal/Modal'
 import { SignUpInForm } from '../../organisms/signUpInForm/signUpInForm'
-import { WordsContainer } from '../../organisms/wordsContainer/wordsContainer'
+import { TextBookBlock } from '../../organisms/TextBookBlock/TextBookBlock'
 import './TextBook.css'
 
 export function TextBook () {
+    const { isAuth } = useContext(authContext)
     const { signInUpModal, openSIU, closeSIU } = useContext(signInUpContext)
     const [words, setWords] = useState<IData[]>([])
-    const [active, setActive] = useState(0)
+    const [active, setActive] = useState((localStorage.getItem('active') != null) ? Number(localStorage.getItem('active')) : 0)
+    const [page, setPage] = useState((localStorage.getItem('page') != null) ? Number(localStorage.getItem('page')) : 1)
     const [isLoading, setLoading] = useState(true)
-    const [page, setPage] = useState(1)
-    const fectchWords = async (activeBtn: number, activePage: number) => {
-        setWords(await Api.getWords(String(activeBtn), String(activePage - 1)))
-    }
-    useEffect(() => {
-        void fectchWords(active, page).then(() => setLoading(false))
-    }, [active, page])
+    const [totalCount, setTotalCount] = useState(600)
+    const fectchUserWords = async (activeBtn: number, activePage: number) => {
+        activeBtn !== 6
+            ? await Api.getAggregatedWords(String(activeBtn), String(activePage - 1), '20').then(data => {
+                setWords(data[0].paginatedResults)
+                setTotalCount(data[0].totalCount[0].count)
+            })
+            : await Api.getAggregatedWords(String(activeBtn), String(1 - 1), '20', 'hard').then(data => setWords(data[0].paginatedResults))
+        }
+    // const fectchWords = async (activeBtn: number, activePage: number) => {
+    //     await Api.getWords(String(activeBtn), String(activePage))
+    // }
+
+        useEffect(() => {
+            // eslint-disable-next-line no-void
+            void fectchUserWords(active, page).then(() => setLoading(false))
+        }, [active, isAuth, page])
 
     return (
         <div className="wrapper">
@@ -34,32 +47,26 @@ export function TextBook () {
                 <h2>RS Lang</h2>
                 <Nav>
                     <span className='nav__link'>Dictionary</span>
-                    <span className='nav__link'>TextBook</span>
                     <span className='nav__link'>Audio Challenge</span>
                     <span className='nav__link'>Sprint</span>
                     <span className='nav__link'>Statistics</span>
                     {/* <Link className='nav__link' to='#'>Why Us</Link> */}
                     {/* <Link className='nav__link' to='#'>Games</Link> */}
                     {/* <Link className='nav__link' to='#'>Team</Link> */}
-                    {/* <Link className='nav__link' to='#'>Textbook</Link> */}
+                    <Link className='nav__link' to='#'>Textbook</Link>
                     </Nav>
-                    <Button
-                    text='Get Started'
-                    type='secondary'
-                    onClick={openSIU}
-                />
+                    { isAuth
+                        ? <NameBlock/>
+                        : <Button
+                            text='Get Started'
+                            type='secondary'
+                            onClick={openSIU}
+                        />}
             </Header>
             <div className='conteiner'>
                 <main className="main">
-                    <div className="pagination">
-                        <Pagination count={30} size="large" page={page} onChange={(_, num) => setPage(num)}/>
-                    </div>
-                    <div className="words-container">
-                        { isLoading ? <CardSkeletonContainer/> : <WordsContainer words={words}/>}
-                        <div className="wordLevel">
-                            <ButtonLevelsBlock active={active} setActive={setActive} />
-                        </div>
-                    </div>
+                    <TextBookBlock page={page} setPage={setPage} isLoading={isLoading} active={active} setActive={setActive} words={words} totalCount={totalCount} />
+                    <Games/>
                 </main>
             </div>
             <Footer />
