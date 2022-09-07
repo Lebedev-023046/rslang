a36a35013a23ebbd0818682ee2ec74ad28c9e987
lebedev-023046 <lebedev.023046@gmail.com>
2022-09-07 15:58:42 +0300

    feat: add necessary props

--- rs-lang/src/components/organisms/wordsContainer/wordsContainer.tsx
+++ rs-lang/src/components/organisms/wordsContainer/wordsContainer.tsx
@@ -4,13 +4,15 @@ import { WordCard } from '../../molecules/WordCard/WordCard'
 interface WordsContainerProps {
     words: IData[]
     active: number
+    isInProgress: boolean
+    isDictionary: boolean
 }
 
-export function WordsContainer ({ words, active }: WordsContainerProps) {
+export function WordsContainer ({ words, active, isInProgress, isDictionary }: WordsContainerProps) {
     return (
         <div className="wordSet">
             { words.map((card) =>
-                <WordCard active={active} card={card} key={card._id}/>
+                <WordCard isDictionary={isDictionary} isInProgress={isInProgress} active={active} card={card} key={card._id}/>
             )}
         </div>
     )
